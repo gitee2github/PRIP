3e30439f74db92625e9197db975c073f9aba0570
wang_zaomentanzhilang <dev02104@linx-info.com>
2023-01-11 14:25:12 +0800

    show.c中实现 查看prip配置文件功能

--- test/tools/priptool/show.c
+++ test/tools/priptool/show.c
@@ -30,6 +30,16 @@ static void show_timeout(void)
 	return;
 }
 
+static void show_config(void)
+{
+	char cmd[1024];
+	char str[16];
+	strcpy(str, "PRIP_CONFIG");
+	snprintf(cmd, sizeof(cmd), "cat %s", PATH_PRIP_CONFIG);
+	exec_config_cmd(cmd, str, 1);
+	return;
+}
+
 int do_show(int argc, char **argv)
 {
 	if(argc < 1)
