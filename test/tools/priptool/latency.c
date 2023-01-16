a2364762fd90051a7d4045f21579b40128b9e5e4
wang_zaomentanzhilang <dev02104@linx-info.com>
2023-01-16 09:33:50 +0800

    latency.c中实现函数包括：计算当前时间，计算校验和，格式打印，初始化哈希表，哈希表的插入和查询

--- test/tools/priptool/latency.c
+++ test/tools/priptool/latency.c
@@ -93,6 +93,144 @@ static struct pcap_handlers g_pcap_h;
 static struct hashtable hash_table;
 static char padding[] = "priptool";
 
+static long timestamp(void)
+{
+	long timestamp;
+	struct timeval tv;
+	
+	gettimeofday(&tv, NULL);
+
+	timestamp = tv.tv_sec * 1000000L + tv.tv_usec; 
+
+	return timestamp;
+}
+
+static unsigned short cal_chksum(unsigned short *addr, int len)
+{
+    int sum=0;
+    int nleft=len;
+    unsigned short *w=addr;
+    unsigned short answer=0;
+
+    while(nleft > 1)
+    {
+        sum+=*w++;
+        nleft-=2;
+    }
+
+    if (nleft == 1)
+    {
+        *(unsigned char *)(&answer)=*(unsigned char *)w;
+        sum+=answer;
+    }
+
+    sum =  (sum>>16) + (sum&0xffff);
+    sum += (sum>>16);
+    answer = ~sum;
+    return answer;
+}
+
+static void red_printf(const char *__format, ...)
+{
+	char *start = "\033[1;31m";
+	char *end = "\033[0m";
+	char format[4096];
+	va_list args;
+
+	snprintf(format, sizeof(format), "%s%s%s\n", \
+			 start, __format, end);
+
+	va_start(args, __format);
+	vprintf(format, args);
+	va_end(args);
+}
+
+
+
+static void init_hashtable(void)
+{
+	int i;
+
+	hash_table.hash_list = malloc(sizeof(struct hash_list) * HASHSZ);
+	if (!hash_table.hash_list)
+	{
+		fprintf(stderr, "Alloc hash table failed : %s\n", \
+				strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	for (i = 0; i < HASHSZ; i++)
+	{
+		INIT_LIST_HEAD(&hash_table.hash_list[i].head);
+	}
+
+	hash_table.bucket_size = HASHSZ;
+}
+
+static struct monitior *node_insert(uint32_t ipaddr1, uint32_t ipaddr2)
+{
+	uint32_t hash;
+	struct monitior *node;
+
+	node = malloc(sizeof(*node));
+	if (!node)
+		return NULL;
+
+	memset(node, 0, sizeof(*node));
+	node->master_ip = ipaddr1;
+	node->slave_ip = ipaddr2;
+	node->id  = getpid();
+	node->seq = g_curr_seq;
+	node->nslave = 2;
+
+	hash = icmp_hashfn(ipaddr1, ipaddr2, node->id, node->seq);
+
+	list_add_tail(&node->hook, &hash_table.hash_list[hash].head);
+
+	pthread_mutex_init(&node->mutex, NULL);
+
+	return node;
+}
+
+static struct monitior *get_node(uint32_t ip1, uint32_t ip2, uint16_t id, uint16_t seq)
+{
+	uint32_t hash;
+	struct monitior *node;
+	struct list_head *list, *next;
+
+	hash = icmp_hashfn(ip1, ip2, id, seq);
+
+	list_for_each_safe(list, next, &hash_table.hash_list[hash].head)
+	{
+		node = list_entry(list, struct monitior, hook);
+		if (!node)
+			continue;
+
+		if (node && node->master_ip == ip1 && node->slave_ip == ip2 &&
+			id == node->id && seq == node->seq)
+		{
+			return node;
+		}
+	}
+
+	return NULL;
+}
+
+
+static const struct cmd *match_cmd(char *opt)
+{
+	const struct cmd *c;
+
+	for (c = cmds; c->cmd; c++)
+	{
+		if (opt && strcmp(c->cmd, opt) == 0)
+			return c;
+	}
+
+	return NULL;
+}
+
+
 
 int do_latency(int argc, char **argv)
 {
